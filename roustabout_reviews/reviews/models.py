7ee9bb98f2fc2f9367b5b0196dd8263c3336a6b1
dimitrios vassilas <dimitrios.vassilas@fdmgroup.com>
2021-03-11 20:58:21 +0000

    Removed uneeded lines/files in reviews app and added migrations for new review models

--- roustabout_reviews/reviews/models.py
+++ roustabout_reviews/reviews/models.py
@@ -1,1 +1,23 @@
+from base.models import TimestampedIdModel
+from releases.models import Release
+
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+class Review(TimestampedIdModel):
+    title = models.CharField(max_length=80)
+    rating = models.DecimalField(decimal_places=1, max_digits=2, validators=[MaxValueValidator(5), MinValueValidator(0)])
+
+    class Meta:
+        abstract = True
+
+class UserReview(Review):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_reviews')
+    release = models.ForeignKey(Release, on_delete=models.CASCADE, related_name='user_reviews')
+    body = models.TextField(max_length=2000)
+
+class ArticleReview(Review):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='articles')
+    release = models.ForeignKey(Release, on_delete=models.CASCADE, related_name='articles')
+    body = models.TextField(max_length=6000)
