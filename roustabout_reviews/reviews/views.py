1152d18c9c091ee766dc7f89fdb176351a102613
dimitrios vassilas <dimitrios.vassilas@fdmgroup.com>
2021-03-12 19:12:16 +0000

    Added review (user/articles) api views

--- roustabout_reviews/reviews/views.py
+++ roustabout_reviews/reviews/views.py
@@ -1,1 +1,12 @@
-from django.shortcuts import render
+from reviews import serializers, models
+
+from rest_framework import viewsets
+
+class UserReviewViewset(viewsets.ModelViewSet):
+    queryset = models.UserReview.objects.all()
+    serializer_class = serializers.UserReviewSerializer
+
+
+class ArticleReviewViewset(viewsets.ModelViewSet):
+    queryset = models.ArticleReview.objects.all()
+    serializer_class = serializers.ArticleReviewSerializer
