a5026d1f75dbfb25909f5f0c73323f069c45576b
dimitrios vassilas <dimitrios.vassilas@fdmgroup.com>
2021-10-31 18:12:02 +0200

    Corrected related field serializers to allow for create and retrieve functionality with least load to db

--- roustabout_reviews/reviews/serializers.py
+++ roustabout_reviews/reviews/serializers.py
@@ -1,4 +1,5 @@
-from reviews import models
+from reviews import models as review_models
+from releases import models as release_models
 
 from django.contrib.auth.models import User
 from rest_framework import serializers
@@ -8,16 +9,16 @@ class UserReviewSerializer(serializers.HyperlinkedModelSerializer):
     author = serializers.SlugRelatedField(queryset=User.objects.all(), many=False, slug_field='username')
 
     class Meta:
-        model = models.UserReview
+        model = review_models.UserReview
         fields = ['id', 'url', 'title', 'rating', 'author', 'release', 'body']
         read_only_fields = ['id']
 
 
 class ArticleReviewSerializer(serializers.HyperlinkedModelSerializer):
-    author = serializers.SlugRelatedField(read_only=True, many=False, slug_field='username')
-    release = serializers.SlugRelatedField(read_only=True, many=False, slug_field='title')
+    author = serializers.SlugRelatedField(queryset=User.objects.all(), many=False, slug_field='username')
+    release = serializers.SlugRelatedField(queryset=release_models.Release.objects.all(), many=False, slug_field='title')
 
     class Meta:
-        model = models.ArticleReview
+        model = review_models.ArticleReview
         fields = ['id', 'url', 'title', 'rating', 'author', 'artists', 'release', 'genre', 'body']
-        read_only_fields = ['id', 'url', 'author', 'release', 'genre']
+        read_only_fields = ['id', 'url', 'genre']
